56bd82c9cc444f340cdc230c1274fe2db3ce9579
sajeevan <sajeevanjspy@gmail.com>
2025-12-07 23:44:40 +0530

    chore: removed unnecessary lines from the macro

--- crates/pg-ephemeral/src/macros.rs
+++ crates/pg-ephemeral/src/macros.rs
@@ -4,22 +4,10 @@ macro_rules! log_trace {
     ($($arg:tt)*) => { tracing::trace!($($arg)*) };
 }
 
-#[cfg(not(feature = "tracing"))]
-#[macro_export]
-macro_rules! log_trace {
-    ($($arg:tt)*) => {};
-}
-
 #[cfg(feature = "tracing")]
 #[macro_export]
 macro_rules! log_debug {
     ($($arg:tt)*) => { tracing::debug!($($arg)*) };
-}
-
-#[cfg(not(feature = "tracing"))]
-#[macro_export]
-macro_rules! log_debug {
-    ($($arg:tt)*) => {};
 }
 
 #[cfg(feature = "tracing")]
@@ -28,32 +16,14 @@ macro_rules! log_info {
     ($($arg:tt)*) => { tracing::info!($($arg)*) };
 }
 
-#[cfg(not(feature = "tracing"))]
-#[macro_export]
-macro_rules! log_info {
-    ($($arg:tt)*) => {};
-}
-
 #[cfg(feature = "tracing")]
 #[macro_export]
 macro_rules! log_warn {
     ($($arg:tt)*) => { tracing::warn!($($arg)*) };
 }
 
-#[cfg(not(feature = "tracing"))]
-#[macro_export]
-macro_rules! log_warn {
-    ($($arg:tt)*) => {};
-}
-
 #[cfg(feature = "tracing")]
 #[macro_export]
 macro_rules! log_error {
     ($($arg:tt)*) => { tracing::error!($($arg)*) };
-}
-
-#[cfg(not(feature = "tracing"))]
-#[macro_export]
-macro_rules! log_error {
-    ($($arg:tt)*) => {};
 }

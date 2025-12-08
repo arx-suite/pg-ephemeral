5abcbf0a821e7bf6278d806c31dd59c9e74903fd
sajeevan <sajeevanjspy@gmail.com>
2025-12-08 10:29:34 +0530

    refactor: replace the Error type to be an interface rather than concrete object

--- crates/pg-ephemeral/src/ephemeral.rs
+++ crates/pg-ephemeral/src/ephemeral.rs
@@ -1,8 +1,8 @@
-use crate::EphemeralResult;
+use std::result::Result;
 
 /// main interface for interacting with the application
-pub trait Ephemeral {
-    fn start(&mut self) -> impl Future<Output = EphemeralResult<()>>;
-    fn is_running(&self) -> impl Future<Output = EphemeralResult<bool>>;
-    fn shutdown(&mut self) -> impl Future<Output = EphemeralResult<()>>;
+pub trait Ephemeral<E: std::error::Error> {
+    fn start(&mut self) -> impl Future<Output = Result<(), E>>;
+    fn shutdown(&mut self) -> impl Future<Output = Result<(), E>>;
+    fn is_running(&self) -> impl Future<Output = Result<bool, E>>;
 }
